ceb9dbcce78aeada951bdfb630405de9f0f05acb
Adam Neuber <adam.neuber.024@pslib.cz>
2025-06-07 18:15:01 +0200

    tecnically works, but really barebones

--- main.ts
+++ main.ts
@@ -1,32 +1,37 @@
+const backMovementEqualizer = 1.85
+const rightTurnElqualizer = 0.8
 
+let speed = 30
+let detecting = false
 
-function movement(speed: number, detecting: boolean) {
+function movement(sp: number, detect: boolean) {
     
     //detect and move obstacle
-    if (TPBot.sonarJudge(TPBot.Sonarjudge.Less, 7) && !detecting) {
-        detecting = true
+    if (TPBot.sonarJudge(TPBot.Sonarjudge.Less, 7) && !detect) {
+        detect = true
         TPBot.setServo(TPBot.ServoTypeList.S360, TPBot.ServoList.S1, 90)
-        TPBot.setTravelTime(TPBot.DriveDirection.Left, speed, 1)
-        TPBot.setTravelTime(TPBot.DriveDirection.Forward, speed, 1)
+        TPBot.setTravelTime(TPBot.DriveDirection.Left, sp, 1)
+        TPBot.setTravelTime(TPBot.DriveDirection.Forward, sp, 1)
         TPBot.setServo(TPBot.ServoTypeList.S360, TPBot.ServoList.S1, 360)
-        TPBot.setTravelTime(TPBot.DriveDirection.Backward, speed, 1.83)
-        TPBot.setTravelTime(TPBot.DriveDirection.Right, speed, 0.78)
+        TPBot.setTravelTime(TPBot.DriveDirection.Backward, sp, backMovementEqualizer)
+        TPBot.setTravelTime(TPBot.DriveDirection.Right, sp, rightTurnElqualizer)
+        detect = false
     }
     
     //follow line
     if (TPBot.trackLine(TPBot.TrackingState.L_R_line)) {
-        TPBot.setWheels(speed, speed)
+        TPBot.setWheels(sp, sp)
     }
     if (TPBot.trackLine(TPBot.TrackingState.L_unline_R_line)) {
-        TPBot.setWheels(speed, 0)
+        TPBot.setWheels(sp, 0)
     }
     if (TPBot.trackLine(TPBot.TrackingState.L_line_R_unline)) {
-        TPBot.setWheels(0, speed)
+        TPBot.setWheels(0, sp)
     }
 }
 
 TPBot.setServo(TPBot.ServoTypeList.S360, TPBot.ServoList.S1, 360)
 
 basic.forever(function () {
-    movement(30, false)
+    movement(speed, detecting)
 })
